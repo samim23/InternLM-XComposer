26555cdd09aa285deecd4b32ee4cb2eba0cb6212
samim23 <s@samim.io>
2024-08-25 20:46:39 +0200

    fix

--- predict.py
+++ predict.py
@@ -1,11 +1,9 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-
 import torch
 from transformers import AutoModel, AutoTokenizer
 from cog import BasePredictor, Input, Path
-
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
@@ -20,10 +18,9 @@ class Predictor(BasePredictor):
             .cuda()
             .eval()
         )
-        tokenizer = AutoTokenizer.from_pretrained(
+        self.tokenizer = AutoTokenizer.from_pretrained(
             "internlm/internlm-xcomposer2-vl-1_8b", trust_remote_code=True
         )
-        self.model.tokenizer = tokenizer
 
     def predict(
         self,
@@ -31,6 +28,10 @@ class Predictor(BasePredictor):
         text: str = Input(description="Input text."),
     ) -> str:
         """Run a single prediction on the model"""
-        output = self.model.generate(text, str(image) if image else None)
-        return output
-
+        query = f"<ImageHere>{text}" if image else text
+        image_path = str(image) if image else None
+        
+        with torch.cuda.amp.autocast():
+            response, _ = self.model.chat(self.tokenizer, query=query, image=image_path, history=[], do_sample=False)
+        
+        return response
